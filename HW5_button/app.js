7f41c2d9765a02dbb2bbc12560d6c2a51b4064a4
mariiapasko <mariia.pasko85@gmail.com>
2023-09-11 22:09:31 +0200

    fixed lastChangedTime

--- HW5_button/app.js
+++ HW5_button/app.js
@@ -38,7 +38,7 @@ button.addEventListener('click', () => {
     button.textContent = isOnDark ? 'Turn on' : 'Turn off';
 
     localStorage.setItem('isOn', isOnDark);
-    localStorage.setItem('lastChangedTime', formattedDate);
+    localStorage.setItem('lastCgangedTime', formattedDate);
 });
 
 function formatDate(date) {
