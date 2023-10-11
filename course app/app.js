03aff97a12adb0160edab1abfcc08a501e01822e
mariiapasko <mariia.pasko85@gmail.com>
2023-10-11 10:00:05 +0200

    made changes in switching between tabs

--- course app/app.js
+++ course app/app.js
@@ -1,21 +1,38 @@
 import { calendar } from "./calendar.js";
 import { holidays } from "./holidays.js";
 
-const tabs = document.querySelectorAll('.tabs div');
+const tabs = document.querySelectorAll('.tab');
+const tabContents = document.querySelectorAll('.tab-content');
+
+function switchTab(tabId) {
+    tabs.forEach(tab => {
+        tab.classList.remove('active');
+    });
+    
+    tabContents.forEach(content => {
+        content.classList.remove('active');
+    });
+
+    const selectedTab = document.getElementById(tabId);
+    const selectedContent = document.getElementById(`${tabId}-content`);
+
+    selectedTab.classList.add('active');
+    selectedContent.classList.add('active');
+}
 
 tabs.forEach(tab => {
-    tab.addEventListener('click', () => {
-        if (tab.classList.contains('active')) {
-            return;
-        }
-
-        tabs.forEach(item => item.classList.remove('active'));
-        tab.classList.add('active');
-
-        if (tab.id === 'tab1') {
-            calendar();
-        } else if (tab.id === 'tab2') {
-            holidays();
+    tab.addEventListener('click', (e) => {
+        if (!e.target.classList.contains('active')) {
+            switchTab(e.target.id);
+            
+            if (e.target.id === 'tab1') {
+                calendar();
+            } else if (e.target.id === 'tab2') {
+                holidays();
+            }
         }
     });
 });
+
+switchTab('tab1');
+calendar();
